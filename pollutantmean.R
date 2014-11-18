ca248b5a986043b146b700fd5971b0ed51ba9e49
Ko-Ying <pengkoying@gmail.com>
2014-11-18 17:56:25 -0500

    Update pollutantmean.R

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,13 +1,27 @@
-setwd("C:/Users/Ko-Ying/Desktop")
 pollutantmean <- function(directory, pollutant, id = 1:332) {
-id <- as.integer(id)
-pollutant <- as.character(pollutant)
-pdata <- c()
-	for(i in id){
-		MyData <- sprintf("%03d.csv",id)
-		MyData <- paste(directory,MyData[i],sep="/")
-		pdata2 <- read.csv(MyData,header=T)
-		pdata <- rbind(pdata,pdata2)
-		}
-	mean(pdata[,pollutant],na.rm=T)
+        ## 'directory' is a character vector of length 1 indicating
+        ## the location of the CSV files
+
+        ## 'pollutant' is a character vector of length 1 indicating
+        ## the name of the pollutant for which we will calculate the
+        ## mean; either "sulfate" or "nitrate".
+
+        ## 'id' is an integer vector indicating the monitor ID numbers
+        ## to be used
+
+        ## Return the mean of the pollutant across all monitors list
+        ## in the 'id' vector (ignoring NA values)
+}
+
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+        directory<-as.character(directory)
+        pollutant<-as.character(pollutant)
+        id<-as.integer(id)
+        pdata1<-c()
+        
+        for (i in seq_along(id)) {
+                pdata2 <- read.csv(paste(directory, "./", formatC(i,flag = "0" ,width = 3 ), pattern=".csv", sep = ""))
+                pdata1 <- c(pdata1, pdata2[[pollutant]])
+        }
+        mean(na.omit(pdata1))
 }
